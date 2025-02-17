bc3e2be11714b68310379ea84198deb044cf2034
Venkat varma <kvenkatvarma@gmail.com>
2025-02-17 16:45:08 +0530

    Add Contact Handler

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -36,6 +36,9 @@ class ContactIndex extends React.Component{
       ]
      }
    }
+   handleAddContact =()=>{
+       alert("Hello");
+   }
       render(){
         return(
                 <div>
@@ -51,7 +54,7 @@ class ContactIndex extends React.Component{
                              </div>
                              <div className="row py-2">
                                  <div className="col-8 offset-2 row">
-                                 <AddContact/>
+                                 <AddContact handleAddContact ={this.handleAddContact}/>
                               </div>
                              </div>
                              <div className="row py-2">
